6a9722a40aad278d04050c62619814ba6f2d4d19
itsmedapny <madaphneeevediente@gmail.com>
2024-05-22 17:10:46 +0800

    Successful chart setup

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from fire.models import Locations, Incident
@@ -7,3 +9,14 @@ class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
+
+class ChartView(ListView):
+    template_name ='chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def get_queryset(self, *args, **kwargs):
+        pass
+
